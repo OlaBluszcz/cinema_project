6957130c6187fa2282621335df6e3d606623c6cb
Aleksandra Bluszcz <a.bluszcz.312@studms.ug.edu.pl>
2025-06-11 22:39:08 +0200

    Initial commit

---
+++ CinemaReservationApi/CinemaReservationApi/Models/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using CinemaReservationApi.Models;
+using System.Text.Json.Serialization;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    public int ScreeningId { get; set; }
+
+    [JsonIgnore]
+    public Screening? Screening { get; set; }
+    public int SeatId { get; set; }
+
+    [JsonIgnore]
+    public Seat? Seat { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;        // DODAJ TO
+    public string PhoneNumber { get; set; } = string.Empty;  // I TO
+    public DateTime ReservationTime { get; set; } = DateTime.Now;
+}
